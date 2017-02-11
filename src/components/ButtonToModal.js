58a390130d3d1a734c0f122dbcdaa7f834578ae6
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-11 09:57:10 +0200

    Add styles to button

--- src/components/ButtonToModal.js
+++ src/components/ButtonToModal.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 class ButtonToModal extends Component {
   render() {
     return (
-      <View>
-        <Text>ButtonToModal</Text>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.pressableArea}>
+          <Text style={styles.text}>OPEN MODAL</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2ecc71',
+  },
+  pressableArea: {
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+  },
+  text: {
+    color: '#ffffff',
+    fontWeight: '500',
+  },
+});
+
 export default ButtonToModal;
