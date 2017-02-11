ce0c2eb7bccf3257adf2a50652fa3d9655eed85b
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-11 10:48:53 +0200

    Add basic working modal

--- src/components/ButtonToModal.js
+++ src/components/ButtonToModal.js
@@ -1,13 +1,79 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Animated, Dimensions} from 'react-native';
 
 class ButtonToModal extends Component {
+  state = {
+    isModalOpen: false,
+    animatedValue: new Animated.Value(0),
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.refs.button.measure((x, y, width, height, pageX, pageY) => {
+        console.log('yello', pageX, pageY);
+        this.setState({
+          originalWidth: width,
+          originalHeight: height,
+          pageX,
+          pageY,
+        })
+      });
+    }, 0);
+  }
+  
+  
+  onPress = () => {
+    const {isModalOpen, animatedValue} = this.state;
+    console.log('oliko auki', isModalOpen);
+    Animated.timing(animatedValue, {toValue: isModalOpen ? 0 : 1}).start();
+    this.setState({isModalOpen: !isModalOpen});
+  }
+  
+  onLayout = ({nativeEvent: {layout}}) => {
+    // console.log(layout);
+    // this.setState({
+    //   originalWidth: layout.width,
+    //   originalHeight: layout.height,
+    // });
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.pressableArea}>
+      <View style={styles.container} ref="button">
+        <TouchableOpacity style={styles.pressableArea} onPress={this.onPress}>
           <Text style={styles.text}>OPEN MODAL</Text>
         </TouchableOpacity>
+        <Animated.View 
+          style={[
+            styles.modal,
+            {
+              width: this.state.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [this.state.originalWidth, Dimensions.get('window').width],
+              }),
+              height: this.state.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [this.state.originalHeight, Dimensions.get('window').height],
+              }),
+              backgroundColor: this.state.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['rgba(46, 204, 113, 0)', 'rgba(46, 204, 113, 1)']
+              }),
+              left: this.state.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -this.state.pageX]
+              }),
+              top: this.state.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -this.state.pageY]
+              }),
+            }
+          ]} 
+          pointerEvents="none" 
+          onLayout={this.onLayout}
+        >
+          <Text style={styles.text}>Modal it self</Text>
+        </Animated.View>
       </View>
     );
   }
@@ -25,6 +91,14 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontWeight: '500',
   },
+  modal: {
+    backgroundColor: 'red',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    // right: 0,
+    // bottom: 0,
+  }
 });
 
 export default ButtonToModal;
