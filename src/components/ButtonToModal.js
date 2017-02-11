c1af5b2e0b3668906167d394ae2004642b5d3c8f
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-02-11 10:54:02 +0200

    Reindent

--- src/components/ButtonToModal.js
+++ src/components/ButtonToModal.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Animated, Dimensions} from 'react-native';
+import {View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity, 
+  Animated, 
+  Dimensions,
+} from 'react-native';
 
 class ButtonToModal extends Component {
   state = {
